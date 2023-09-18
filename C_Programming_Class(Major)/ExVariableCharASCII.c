6a0e43d895d0b973850b40767e7ecb67d33b5f12
Kihyun Ko <lkorewa101@gmail.com>
2023-09-18 23:28:08 +0900

    Update and rename C_Programming_Class(Major)/ExVariableCharASCII.c to C_Programming_Class(Major)/Week 2/ExVariableCharASCII.c

--- C_Programming_Class(Major)/ExVariableCharASCII.c
+++
@@ -1,8 +0,0 @@
-/* 정수형과 문자형으로 사용하는 char형 예제 */
-#include <stdio.h>
-int main() {
-	char ch = 'A';
-	printf("%c의 아스키 값은 %d이다.\n", ch + 10, ch + 10);
-	printf("%c의 아스키 값은 %d이다.\n", ch + 25, ch + 25);
-	return 0;
-}
